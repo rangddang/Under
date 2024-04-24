a0c3ff786343a47544d4b2211c0991ede66122d1
Rang_ddang <62083659+rangddang@users.noreply.github.com>
2024-04-24 23:15:51 +0900

    Add Attack Animation

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Camera camera;
+    public Transform target;
+
+    [SerializeField] private float cameraSpeed;
 
     private void Awake()
     {
         camera = GetComponent<Camera>();
     }
 
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, cameraSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,8 +12,16 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpPower;
 
+    [SerializeField] private AnimationClip[] attackAnim;
+
     private float direction;
     private int saveGra;
+    private int attackNum;
+    private float attackTime;
+    private float currentAttackLate;
+
+    private bool isTryAttack;
+    private bool isAttacking;
 
     private void Awake()
     {
@@ -26,14 +34,32 @@ public class PlayerController : MonoBehaviour
     {
         direction = Input.GetAxisRaw("Horizontal");
 
-        transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (currentAttackLate <= attackTime)
         {
-            rigid.velocity = Vector3.up * jumpPower;
+            transform.position += Vector3.right * direction * moveSpeed * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid.velocity = Vector3.up * jumpPower;
+            }
+            if (direction > 0)
+            {
+                sprite.flipX = true;
+            }
+            else if (direction < 0)
+            {
+                sprite.flipX = false;
+            }
         }
 
-        sprite.flipX = direction > 0;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StopCoroutine("TryAttack");
+            StartCoroutine("TryAttack");
+        }
+
+        attackTime += Time.deltaTime;
+
+        
         anim.SetBool("IsRunning", direction != 0);
         int gravity = 0;
         if(rigid.velocity.y > 0)
@@ -49,11 +75,80 @@ public class PlayerController : MonoBehaviour
             anim.Play("Player_Onground", -1, 0);
         }
         saveGra = gravity;
+        
+        Attack();
         anim.SetInteger("Gravity", gravity);
     }
 
     public bool IsOnGrounded()
     {
         return true;
+    }
+
+    public void Attack()
+    {
+        if (isTryAttack)
+        {
+            if (currentAttackLate <= attackTime)
+            {
+                // Attack
+                //switch (attackNum)
+                //{
+                //    case 0: Attack_1(); break;
+                //    case 1: Attack_2(); break;
+                //    case 2: Attack_3(); break;
+                //    case 3: Attack_4(); break;
+                //}
+                anim.Play("Attack_" +  (attackNum + 1), -1, 0);
+                StopCoroutine("OnAttack");
+                StartCoroutine("OnAttack");
+                currentAttackLate = attackAnim[attackNum].length;
+                attackTime = 0;
+
+                if (direction > 0)
+                {
+                    sprite.flipX = true;
+                }
+                else if (direction < 0)
+                {
+                    sprite.flipX = false;
+                }
+
+                attackNum++;
+                if(attackNum >= 4) attackNum = 0;
+            }
+        }
+    }
+
+    //public void Attack_1()
+    //{
+
+    //}
+    //public void Attack_2()
+    //{
+
+    //}
+    //public void Attack_3()
+    //{
+
+    //}
+    //public void Attack_4()
+    //{
+
+    //}
+
+    private IEnumerator TryAttack()
+    {
+        isTryAttack = true;
+        yield return new WaitForSeconds(0.2f);
+        isTryAttack = false;
+    }
+
+    private IEnumerator OnAttack()
+    {
+        //anim.SetBool("IsAttacking", true);
+        yield return new WaitForSeconds(0.7f);
+        //anim.SetBool("IsAttacking", false);
+        attackNum = 0;
     }
 }
